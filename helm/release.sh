87d5d04768d5d51aac966309402430a421dcf4e5
Karsten Müller <kmue@me.com>
2020-05-12 16:17:31 +0200

    changes

--- helm/release.sh
+++ helm/release.sh
@@ -5,6 +5,18 @@ set -e
 set -o nounset
 set -o pipefail
 
-helm package helm/
-mv *.tgz docs/
-helm repo index docs/ --url https://github.io/fielmann-ag/version-monitor/docs
+DEST="../.deploy"
+
+helm package . --destination "$DEST"
+
+# helm repo index docs/ --url https://github.io/fielmann-ag/version-monitor/docs
+
+export CR_OWNER=karstenmueller
+export CR_CHARTS_REPO=https://github.com/karstenmueller/version-monitor/
+export CR_GIT_REPO=version-monitor
+export CR_PACKAGE_PATH="$DEST"
+# export CR_TOKEN="123456789"
+# export CR_GIT_BASE_URL="https://api.github.com/"
+# export CR_GIT_UPLOAD_URL="https://uploads.github.com/"
+
+cr index
