c8123960c8037a5269e6998d433c818f5429f98c
Karsten Müller <kmue@me.com>
2020-05-12 16:35:37 +0200

    changes

--- helm/release.sh
+++ helm/release.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-set -x
 set -e
 set -o nounset
 set -o pipefail
 
-DEST="../.deploy"
+script_dir=$(
+    cd "$(dirname "$0")"
+    pwd -P
+)
+deploy_dir="$script_dir/../.deploy"
 
-helm package . --destination "$DEST"
+os_name=$(uname | tr '[:upper:]' '[:lower:]')
+curl -sSL https://github.com/helm/chart-releaser/releases/download/v0.2.3/chart-releaser_0.2.3_"$os_name"_amd64.tar.gz |
+    tar xzf -C "$deploy_dir" -
+
+helm package . --destination "$deploy_dir"
 
 # helm repo index docs/ --url https://github.io/fielmann-ag/version-monitor/docs
 
 export CR_OWNER=karstenmueller
 export CR_CHARTS_REPO=https://github.com/karstenmueller/version-monitor/
 export CR_GIT_REPO=version-monitor
-export CR_PACKAGE_PATH="$DEST"
-# export CR_TOKEN="123456789"
-# export CR_GIT_BASE_URL="https://api.github.com/"
-# export CR_GIT_UPLOAD_URL="https://uploads.github.com/"
+export CR_PACKAGE_PATH="$deploy_dir"
+export CR_TOKEN="$GH_TOKEN" # from environment
 
-cr index
+"$deploy_dir"/cr upload
